82d048740fbe2ed910f6d906ee908dd2735533d8
SheilaNS <shei.nsantos@gmail.com>
2021-12-14 19:56:01 -0300

    Requisitos 7 e 8 ok

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Requisito 5
 const listaTarefas = document.getElementById('lista-tarefas');
 const botaoCriar = document.getElementById('criar-tarefa');
 const textoTarefa = document.getElementById('texto-tarefa');
+const itemLista = document.getElementsByClassName('item-tarefa');
 
+// Requisito 7 e 8: Ajuda obtida com o Guilherme Azevedo
+function selecionaTarefa(event) {
+  for (let i = 0; i < itemLista.length; i += 1) {
+    itemLista[i].classList.remove('selecionado');
+    itemLista[i].style.backgroundColor = 'white';
+  }
+  const evento = event.target;
+  evento.classList.add('selecionado');
+  evento.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+// Requisito 5
 function criaTarefa() {
   const tarefaItem = document.createElement('li');
-  tarefaItem.className = 'item-terefa';
+  tarefaItem.className = 'item-tarefa';
   tarefaItem.innerText = textoTarefa.value;
+  tarefaItem.addEventListener('click', selecionaTarefa);
   listaTarefas.appendChild(tarefaItem);
   textoTarefa.value = '';
 }
